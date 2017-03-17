7553c63a3a93c0086ecc814c1031b26521a87028
Miquel Garriga <gbmiquel@gmail.com>
2017-03-17 23:20:32 +0100

    Add an option to supress dat files creation

--- opj2dat.cpp
+++ opj2dat.cpp
@@ -2,6 +2,7 @@
     File                 : opj2dat.cpp
     --------------------------------------------------------------------
     Copyright            : (C) 2008 Stefan Gerlach
+                           (C) 2017 Miquel Garriga
     Email (use @ for *)  : stefan.gerlach*uni-konstanz.de
     Description          : Origin project converter
 
@@ -11,7 +12,7 @@
  *                                                                         *
  *  This program is free software; you can redistribute it and/or modify   *
  *  it under the terms of the GNU General Public License as published by   *
- *  the Free Software Foundation; either version 2 of the License, or      *
+ *  the Free Software Foundation; either version 3 of the License, or      *
  *  (at your option) any later version.                                    *
  *                                                                         *
  *  This program is distributed in the hope that it will be useful,        *
@@ -36,20 +37,24 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-	if(argc != 2) {
-		cout << "Usage : ./opj2dat <file.opj>" << endl;
+	if (argc < 2) {
+		cout << "Usage : ./opj2dat [--check-only] <file.opj>" << endl;
 		return -1;
 	}
 
 	cout << "opj2dat " << LIBORIGIN_VERSION_STRING << ", Copyright (C) 2008 Stefan Gerlach, 2017 Miquel Garriga" << endl;
 
-	if(!strcmp(argv[1],"-v"))
+	if (string(argv[1]) == "-v")
 		return 0;
 
-	OriginFile opj(argv[1]);
+	bool write_spreads = true;
+	if ((argc > 2) && (string(argv[1]) == "--check-only")) write_spreads = false;
+
+	string inputfile=argv[argc-1];
+	OriginFile opj(inputfile);
 	int status = opj.parse();
 	cout << "Parsing status = " << status << endl;
-	cout << "OPJ PROJECT \"" << argv[1] << "\" VERSION = " <<  opj.version() << endl;
+	cout << "OPJ PROJECT \"" << inputfile.c_str() << "\" VERSION = " <<  opj.version() << endl;
 
 	cout << "number of spreadsheets = " << opj.spreadCount() << endl;
 	cout << "number of matrixes     = " << opj.matrixCount() << endl;
@@ -69,47 +74,49 @@ int main(int argc, char *argv[]) {
 			cout << "	Column " << (j+1) << " : " << column.name.c_str() << " / type : " << column.type << ", rows : " << spread.maxRows << endl;
 		}
 
-		ostringstream sfilename;
-		sfilename << argv[1] << "." << (s+1) << ".dat";
-		cout << "saved to " << sfilename.str() << endl;
+		if (write_spreads) {
+			ostringstream sfilename;
+			sfilename << inputfile.c_str() << "." << (s+1) << ".dat";
+			cout << "saved to " << sfilename.str() << endl;
 
-		ofstream outf;
-		outf.open(sfilename.str().c_str(), ios_base::out);
-		if (!outf.good()) {
-			cout << "Could not open " << sfilename.str() << endl;
-			return -1;
-		}
-		// header
-		for (int j=0;j<columnCount;j++) {
-		    outf << spread.columns[j].name.c_str() << "; ";
-			cout << spread.columns[j].name.c_str();
-		}
-		outf << endl;
-		cout << endl << " Data: " << endl;
-		// data
-		for (int i=0;i<(int)spread.maxRows;i++) {
-			for (int j=0;j<columnCount;j++) {
-				if (i<(int)spread.columns[j].data.size()) {
-					Origin::variant value=spread.columns[j].data[i];
-					double v=0.;
-					if (value.type() == typeid(double)) {
-						v = boost::get<double>(value);
-						if (v != -1.23456789e-300) {
-							outf << v << "; ";
-						} else {
-							outf << nan("NaN") << "; ";
-						}
-					}
-					if (value.type() == typeid(string)) {
-						outf << boost::get<string>(value).c_str() << "; ";
-					}
-				} else {
-					outf << "; ";
-				}
+			ofstream outf;
+			outf.open(sfilename.str().c_str(), ios_base::out);
+			if (!outf.good()) {
+				cout << "Could not open " << sfilename.str() << endl;
+				return -1;
 			}
-		   outf << endl;
+			// header
+			for (int j=0;j<columnCount;j++) {
+				outf << spread.columns[j].name.c_str() << "; ";
+				cout << spread.columns[j].name.c_str();
+			}
+			outf << endl;
+			cout << endl << " Data: " << endl;
+			// data
+			for (int i=0;i<(int)spread.maxRows;i++) {
+				for (int j=0;j<columnCount;j++) {
+					if (i<(int)spread.columns[j].data.size()) {
+						Origin::variant value=spread.columns[j].data[i];
+						double v=0.;
+						if (value.type() == typeid(double)) {
+							v = boost::get<double>(value);
+							if (v != _ONAN) {
+								outf << v << "; ";
+							} else {
+								outf << nan("NaN") << "; ";
+							}
+						}
+						if (value.type() == typeid(string)) {
+							outf << boost::get<string>(value).c_str() << "; ";
+						}
+					} else {
+						outf << "; ";
+					}
+				}
+			outf << endl;
+			}
+			outf.close();
 		}
-		outf.close();
 	}
 	return 0;
 }
